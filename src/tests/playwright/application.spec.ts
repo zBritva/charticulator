9676077bc829658a2781d0c77cafbc3b846aad4c
Ilfat Galiev <zBritva@users.noreply.github.com>
2024-09-26 11:01:00 -0700

    Test results (#58)
    * Add screenshots to test results
    
    * Update node.js.yml
    
    * continue-on-error

--- src/tests/playwright/application.spec.ts
+++ src/tests/playwright/application.spec.ts
@@ -2,14 +2,16 @@ import { test, expect } from '@playwright/test';
 import * as path from "path";
 import { expectShapesCount, loadChart, pathPrefix } from './utils';
 
-test('has title Charticulator', async ({ page }) => {
+test('has title Charticulator', async ({ page }, testInfo) => {
   await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Charticulator/);
+
+  await takeScreenshot(testInfo, page);
 });
 
-test('opens file view', async ({ page }) => {
+test('opens file view', async ({ page }, testInfo) => {
   await page.goto('/');
 
   // Click the get started link.
@@ -22,47 +24,70 @@ test('opens file view', async ({ page }) => {
 
   let fileViewTabs = await page.getByTestId('file-view-tabs');
 
+  await takeScreenshot(testInfo, page);
+
   await expect(fileViewTabs).toBeVisible();
 });
 
-test('open mushrooms', async ({ page }) => {
+test('open mushrooms', async ({ page }, testInfo) => {
   await page.goto('/');
   const chartFilePath = `${pathPrefix}/mushrooms.chart`;
   await loadChart(page, chartFilePath);
+
+  await takeScreenshot(testInfo, page);
   await expectShapesCount(page, 412);
 });
 
-test('open bubble_chart', async ({ page }) => {
+test('open bubble_chart', async ({ page }, testInfo) => {
   await page.goto('/');
   const chartFilePath = `${pathPrefix}/bubble_chart.chart`;
   await loadChart(page, chartFilePath);
+
+  await takeScreenshot(testInfo, page);
   await expectShapesCount(page, 148);
 });
 
-test('open bump_chart', async ({ page }) => {
+test('open bump_chart', async ({ page }, testInfo) => {
   await page.goto('/');
   const chartFilePath = `${pathPrefix}/bump_chart.chart`;
   await loadChart(page, chartFilePath);
+
+  await takeScreenshot(testInfo, page);
   await expectShapesCount(page, 812);
 });
 
-test('open nightingale', async ({ page }) => {
+test('open nightingale', async ({ page }, testInfo) => {
   await page.goto('/');
   const chartFilePath = `${pathPrefix}/nightingale.chart`;
   await loadChart(page, chartFilePath);
+
+  await takeScreenshot(testInfo, page);
   await expectShapesCount(page, 84);
 });
 
-test('open 200_Mushrooms', async ({ page }) => {
+test('open 200_Mushrooms', async ({ page }, testInfo) => {
   await page.goto('/');
   const chartFilePath = `${pathPrefix}/200_Mushrooms.chart`;
   await loadChart(page, chartFilePath);
+
+  await takeScreenshot(testInfo, page);
   await expectShapesCount(page, 344);
 });
 
-test('open World_Population_2017', async ({ page }) => {
+test('open World_Population_2017', async ({ page }, testInfo) => {
   await page.goto('/');
   const chartFilePath = `${pathPrefix}/World_Population_2017.chart`;
   await loadChart(page, chartFilePath);
+
+  await takeScreenshot(testInfo, page);
   await expectShapesCount(page, 94);
 });
+
+async function takeScreenshot(testInfo, page) {
+  // Get a unique place for the screenshot.
+  const screenshotPath = testInfo.outputPath(`page.png`);
+  // Add it to the report
+  testInfo.attachments.push({ name: 'screenshot', path: screenshotPath, contentType: 'image/png' });
+  // Take the screenshot itself.
+  await page.screenshot({ path: screenshotPath, timeout: 5000 });
+}
