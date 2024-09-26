f279eaab1bd022ac53a58b236193f7e3db1f9a00
Ilfat Galiev <zbritva@gmail.com>
2024-09-26 10:38:15 -0800

    Add screenshots to test results

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [["html", { outputFile: 'test-results.html' } ]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -37,6 +37,12 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
+
+  expect: {
+    timeout: 3000,
+  },
+
+  timeout: 10000,
 
   /* Configure projects for major browsers */
   projects: [
